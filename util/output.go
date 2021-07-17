9675ce78d04f56f170fd37b66733377eaa52b7d2
thatmattlove <matt@stellar.tech>
2021-07-17 08:03:55 -0700

    Use logger for error checking

--- util/output.go
+++ util/output.go
@@ -3,13 +3,15 @@ package util
 import (
 	"fmt"
 	"os"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // Check checks an error, and if present, logs an error message to the console & exits.
 func Check(m string, e error, format ...interface{}) {
 	args := append(format, e)
 	if e != nil {
-		fmt.Println(fmt.Errorf(m+"\n%v", args...))
+		log.Error(fmt.Sprintf(m+"\n%v", args...))
 		os.Exit(1)
 	}
 }
